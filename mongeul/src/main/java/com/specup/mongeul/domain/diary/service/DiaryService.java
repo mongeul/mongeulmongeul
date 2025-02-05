36687c9537c879def8bb67b99fdbc481dca861a9
김호준 <didl95@naver.com>
2025-02-05 16:19:01 +0900

    feat: 일기 예외처리 구현

--- mongeul/src/main/java/com/specup/mongeul/domain/diary/service/DiaryService.java
+++ mongeul/src/main/java/com/specup/mongeul/domain/diary/service/DiaryService.java
@@ -5,6 +5,8 @@ import com.specup.mongeul.domain.diary.dto.request.DiaryUpdateRequest;
 import com.specup.mongeul.domain.diary.dto.response.DiaryResponse;
 import com.specup.mongeul.domain.diary.entity.Diary;
 import com.specup.mongeul.domain.diary.repository.DiaryRepository;
+import com.specup.mongeul.global.error.CustomException;
+import com.specup.mongeul.global.error.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,7 +28,8 @@ public class DiaryService {
 
     @Transactional
     public DiaryResponse update(Long diaryId, DiaryUpdateRequest request) {
-        Diary diary = diaryRepository.findById(diaryId).orElseThrow();
+        Diary diary = diaryRepository.findById(diaryId)
+                .orElseThrow(() -> new CustomException(ErrorCode.DIARY_NOT_FOUND));
         diary.update(request.getTitle(), request.getContent(), request.isLocked(),
                 request.getPicture(), request.getWeather(), request.getFeeling(),
                 request.getIsPrivate());
@@ -35,12 +38,15 @@ public class DiaryService {
 
     @Transactional(readOnly = true)
     public DiaryResponse read(Long diaryId) {
-        return DiaryResponse.from(diaryRepository.findById(diaryId).orElseThrow());
+        return DiaryResponse.from(diaryRepository.findById(diaryId)
+                .orElseThrow(() -> new CustomException(ErrorCode.DIARY_NOT_FOUND)));
     }
 
     @Transactional
     public void delete(Long diaryId) {
-        Diary diary = diaryRepository.findById(diaryId).orElseThrow();
+        Diary diary = diaryRepository.findById(diaryId)
+                .orElseThrow(() -> new CustomException(ErrorCode.DIARY_NOT_FOUND));
         diary.delete();
+        diaryRepository.save(diary);
     }
 }
