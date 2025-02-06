cba3495bd3d496922f1dfafc6db5e5fd819dba27
김호준 <didl95@naver.com>
2025-02-06 14:43:58 +0900

    feat: 일기 하루에 1개 검증 로직 구현

--- mongeul/src/main/java/com/specup/mongeul/domain/diary/repository/DiaryRepository.java
+++ mongeul/src/main/java/com/specup/mongeul/domain/diary/repository/DiaryRepository.java
@@ -4,9 +4,12 @@ import com.specup.mongeul.domain.diary.entity.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
     List<Diary> findByUserId(Long userId);
+    Optional<Diary> findByUserIdAndCreatedAtBetween(Long userId, LocalDateTime start, LocalDateTime end);
 }
