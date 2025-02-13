428dfa45e14988226f3d5220fe3343e6b2ed6b62
진아영 <ay8160@daum.net>
2025-02-13 15:05:05 +0900

    feat: svg 설정

--- mongeul/src/components/Header.tsx
+++ mongeul/src/components/Header.tsx
@@ -1,11 +1,20 @@
+"use client";
+
 import Link from "next/link";
+import NotificationIcon from "@/assets/icons/notification.svg";
 
 export default function Header() {
   return (
     <div className="w-full flex justify-center">
       <div className="w-full md:w-3/4 flex justify-between p-4">
         <Link href="/">몽글몽글</Link>
-        <Link href="/notification">알림</Link>
+        <Link href="/notification">
+          <NotificationIcon
+            className="w-6 h-6 text-zinc-400"
+            width={24}
+            height={24}
+          />
+        </Link>
       </div>
     </div>
   );
