53997dad08078a5717205ef6eb0e2bba0854ab8e
김호준 <didl95@naver.com>
2025-02-14 12:52:25 +0900

    refactor: 이모지 등록/삭제 분리 작업 (기존 토글형태 동시성 문제)

--- mongeul/src/main/java/com/specup/mongeul/domain/diaryemoji/controller/DiaryEmojiController.java
+++ mongeul/src/main/java/com/specup/mongeul/domain/diaryemoji/controller/DiaryEmojiController.java
@@ -1,12 +1,10 @@
 package com.specup.mongeul.domain.diaryemoji.controller;
 
-import com.specup.mongeul.domain.diaryemoji.dto.request.DiaryEmojiRequest;
 import com.specup.mongeul.domain.diaryemoji.service.DiaryEmojiService;
 import com.specup.mongeul.domain.user.entity.User;
 import com.specup.mongeul.global.common.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -19,14 +17,37 @@ import org.springframework.web.bind.annotation.*;
 public class DiaryEmojiController {
     private final DiaryEmojiService diaryEmojiService;
 
-    @Operation(summary = "이모지 등록/삭제", description = "토글 형식의 이모지 등록 및 삭제")
-    @PostMapping("/diaries/{diaryId}/emojis")
-    public ResponseEntity<ApiResponse<Void>>add(@AuthenticationPrincipal User user, @PathVariable Long diaryId, @Valid @RequestBody DiaryEmojiRequest request) {
-        boolean isAdded = diaryEmojiService.emojiToggle(user.getId(), diaryId, request);
-        if (isAdded) {
-            return ResponseEntity.ok(ApiResponse.success(null, "이모지 추가 성공"));
-        } else {
-            return ResponseEntity.ok(ApiResponse.success(null, "이모지 삭제 성공"));
-        }
+    @Operation(summary = "이모지 추가", description = "이모지를 추가합니다.")
+    @PostMapping("/feeds/{feedId}/emojis/emojiId")
+    public ResponseEntity<ApiResponse<Void>> addEmoji(
+            @AuthenticationPrincipal User user,
+            @PathVariable Long feedId,
+            @PathVariable Long emojiId) {
+        diaryEmojiService.addEmoji(feedId, emojiId, user.getId());
+        return ResponseEntity.ok(ApiResponse.success(null, "이모지 추가 성공"));
     }
+
+    @Operation(summary = "이모지 삭제", description = "이모지를 삭제합니다.")
+    @DeleteMapping("/feeds/{feedId}/emojis")
+    public ResponseEntity<ApiResponse<Void>> deleteEmoji(
+            @AuthenticationPrincipal User user,
+            @PathVariable Long feedId,
+            @PathVariable Long emojiId) {
+        diaryEmojiService.deleteEmoji(feedId, emojiId, user.getId());
+        return ResponseEntity.noContent().build();
+    }
+
+    /**
+     * 토글 형식의 이모지 등록/삭제 ver -> 이후 성능 테스트 비교
+     */
+//    @Operation(summary = "이모지 등록/삭제", description = "토글 형식의 이모지 등록 및 삭제")
+//    @PostMapping("/diaries/{diaryId}/emojis")
+//    public ResponseEntity<ApiResponse<Void>>add(@AuthenticationPrincipal User user, @PathVariable Long diaryId, @Valid @RequestBody DiaryEmojiRequest request) {
+//        boolean isAdded = diaryEmojiService.emojiToggle(user.getId(), diaryId, request);
+//        if (isAdded) {
+//            return ResponseEntity.ok(ApiResponse.success(null, "이모지 추가 성공"));
+//        } else {
+//            return ResponseEntity.ok(ApiResponse.success(null, "이모지 삭제 성공"));
+//        }
+//    }
 }
