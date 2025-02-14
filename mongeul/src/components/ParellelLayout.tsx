8e9dbe35c4c7e24a6df0f896f5a9419ac44a9962
진아영 <ay8160@daum.net>
2025-02-14 22:31:36 +0900

    feat: 레이아웃 적용

--- mongeul/src/components/ParellelLayout.tsx
+++ mongeul/src/components/ParellelLayout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import DefaultLayout from "./DefaultLayout";
 
 interface ParellelLayoutProps {
   children: ReactNode;
@@ -12,8 +13,12 @@ export default function ParellelLayoutProps({
   // TODO 모바일 반응형 children / detail 하나만 보이기, 헤더 뒤로가기 추가
   return (
     <div className="flex gap-4 w-full justify-center">
-      <div className="w-full md:w-1/2">{children}</div>
-      <div className="w-full md:w-1/2">{detail}</div>
+      <div className="w-full md:w-1/2">
+        <DefaultLayout>{children}</DefaultLayout>
+      </div>
+      <div className="w-full md:w-1/2">
+        <DefaultLayout>{detail}</DefaultLayout>
+      </div>
     </div>
   );
 }
