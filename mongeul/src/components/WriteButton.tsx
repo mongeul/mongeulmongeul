7a02df64cdfb0affe1012fada7db9ffbc83cb8a5
진아영 <ay8160@daum.net>
2025-02-14 21:56:29 +0900

    feat: 일기 작성 버튼 추가

--- mongeul/src/components/WriteButton.tsx
+++ mongeul/src/components/WriteButton.tsx
@@ -1,14 +1,48 @@
 "use client";
+
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import WriteButtonDetail from "./diary-write/WriteButtonDetail";
+import DiaryIcon from "@/assets/icons/diary.svg";
 import WriteIcon from "@/assets/icons/write.svg";
+import SharedDiaryIcon from "@/assets/icons/shared-diary.svg";
 
 export default function WriteButton() {
-  const writeDiary = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const writeDiary = (): void => {
     [console.log("다이어리 작성 버튼 클릭")];
+    setIsOpen((prev: boolean) => !prev);
+  };
+
+  const writePersonalDiary = (): void => {
+    console.log("개인 다이어리 작성");
+  };
+  const writeSharedDiary = (): void => {
+    console.log("공유 다이어리 작성");
   };
 
   return (
-    <button onClick={writeDiary}>
-      <WriteIcon className="absolute -top-4 left-1/2 -translate-x-1/2 w-14 h-14 text-theme-400" />
-    </button>
+    <div className="flex flex-col items-center">
+      <AnimatePresence>
+        {isOpen && (
+          <div className="absolute bottom-20 flex flex-row gap-4">
+            <WriteButtonDetail
+              text="나의 일기 작성"
+              icon={<DiaryIcon className="w-7 h-7 text-theme-400" />}
+              onClick={writePersonalDiary}
+            />
+            <WriteButtonDetail
+              text="공유 일기 작성"
+              icon={<SharedDiaryIcon className="w-7 h-7 text-theme-400" />}
+              onClick={writeSharedDiary}
+            />
+          </div>
+        )}
+      </AnimatePresence>
+      <button onClick={writeDiary}>
+        <WriteIcon className="absolute left-1/2 -translate-x-1/2 -top-4 w-14 h-14 text-theme-400" />
+      </button>
+    </div>
   );
 }
