4e6063558aa534b8e52ace73c8a75fba4f60fadf
김호준 <didl95@naver.com>
2025-02-16 12:13:13 +0900

    refactor: BaseSoftDelete isDeleted -> deleted 로 수정

--- mongeul/src/main/java/com/specup/mongeul/global/common/BaseSoftDeleteEntity.java
+++ mongeul/src/main/java/com/specup/mongeul/global/common/BaseSoftDeleteEntity.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 public class BaseSoftDeleteEntity extends BaseTimeEntity {
 
     @Column(columnDefinition = "BOOLEAN DEFAULT false")
-    protected boolean isDeleted;
+    protected boolean deleted;
 
     protected LocalDateTime deletedAt;
 }
