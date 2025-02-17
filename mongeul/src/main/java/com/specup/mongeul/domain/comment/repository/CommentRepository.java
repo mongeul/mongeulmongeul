04aac5393921d5b2d4258ccc3cd5557935261df9
김호준 <didl95@naver.com>
2025-02-18 02:36:37 +0900

    refactor: 댓글 부분 URI diary -> feed로 변경

--- mongeul/src/main/java/com/specup/mongeul/domain/comment/repository/CommentRepository.java
+++ mongeul/src/main/java/com/specup/mongeul/domain/comment/repository/CommentRepository.java
@@ -3,8 +3,6 @@ package com.specup.mongeul.domain.comment.repository;
 import com.specup.mongeul.domain.comment.entity.Comment;
 import com.specup.mongeul.domain.diary.entity.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,17 +13,4 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByDiary(Diary diary);
 
     int countByParentCommentId(Long parentCommentId);
-
-//    @Query(
-//            value = "select count(*) from (" +
-//                    "   select comment_id from comment " +
-//                    "   where parent_comment_id = :parentCommentId " +
-//                    "   limit :limit " +
-//                    ") t",
-//            nativeQuery = true
-//    )
-//    Long countBy(
-//            @Param("parentCommentId") Long parentCommentId,
-//            @Param("limit") Long limit
-//    );
 }
