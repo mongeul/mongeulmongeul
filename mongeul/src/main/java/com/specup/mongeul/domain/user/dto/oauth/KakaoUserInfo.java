ad7cfc4f82a6c25c9f8588b7a442bfa0d842ec90
최승탁 <tmdxkr5@hanmail.net>
2025-02-21 10:10:42 +0900

    feat: Kakao 로그인을 위한 client 생성

---
+++ mongeul/src/main/java/com/specup/mongeul/domain/user/dto/oauth/KakaoUserInfo.java
@@ -0,0 +1,12 @@
+package com.specup.mongeul.domain.user.dto.oauth;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class KakaoUserInfo {
+    private String id;
+    private String nickname;
+    private String email;
+}
