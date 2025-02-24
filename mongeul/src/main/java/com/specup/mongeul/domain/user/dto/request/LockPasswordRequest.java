bcee16d19d19687bb3db61a4b404223ed1929465
최승탁 <tmdxkr5@hanmail.net>
2025-02-24 21:13:38 +0900

    feat: 잠금비밀번호 4자리 필수 설정 구현

--- mongeul/src/main/java/com/specup/mongeul/domain/user/dto/request/LockPasswordRequest.java
+++ mongeul/src/main/java/com/specup/mongeul/domain/user/dto/request/LockPasswordRequest.java
@@ -1,5 +1,6 @@
 package com.specup.mongeul.domain.user.dto.request;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,5 +8,6 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class LockPasswordRequest {
+    @Pattern(regexp = "^[0-9]{4}$", message = "잠금 비밀번호는 4자리 숫자만 가능합니다")
     private String lockPassword;
 }
