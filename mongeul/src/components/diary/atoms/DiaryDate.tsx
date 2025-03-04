2f09b1a94f57287a619c6fa1d63692254137cd01
seungmin <tmdals77117@naver.com>
2025-03-05 00:48:55 +0900

    refactor: 다이어리 아이콘 출력 수정

--- mongeul/src/components/diary/atoms/DiaryDate.tsx
+++ mongeul/src/components/diary/atoms/DiaryDate.tsx
@@ -1,12 +1,31 @@
 import { useSelector } from "react-redux";
 import { RootState } from "@/store/store";
 
+const getDateofMonth = (dateString: string) => {
+  const date = new Date(dateString);
+  return String(date.getDate()).padStart(2, "0");
+};
+
+const getDayofWeek = (dateString: string) => {
+  const date = new Date(dateString);
+  return new Intl.DateTimeFormat("ko-KR", { weekday: "short" }).format(date);
+};
+
 const DiaryDate: React.FC = () => {
   const { selectedDiary } = useSelector((state: RootState) => state.calendar);
 
   if (!selectedDiary) return null;
 
-  return <p>{selectedDiary.date}</p>;
+  const date = getDateofMonth(selectedDiary.date);
+  const day = getDayofWeek(selectedDiary.date);
+
+  return (
+    <p>
+      {date}
+      <span className="m-2">|</span>
+      {day}
+    </p>
+  );
 };
 
 export default DiaryDate;
--- mongeul/src/components/diary/organisms/Diary.tsx
+++ mongeul/src/components/diary/organisms/Diary.tsx
@@ -20,7 +20,7 @@ const Diary: React.FC<DiaryProps> = ({ diary }) => {
     <div>
       {selectedDiary ? (
         <Card height="min-h-[200px] lg:min-h-[450px]">
-          <div className="flex flex-col items-start w-full">
+          <div className="flex flex-col items-start w-full p-4">
             <DiaryHeader />
             <DiaryTitle />
             <DiaryImage />
