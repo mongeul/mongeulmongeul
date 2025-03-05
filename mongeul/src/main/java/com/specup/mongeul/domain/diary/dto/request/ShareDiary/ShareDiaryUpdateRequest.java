e99e1efef4b8f4272e94b4244b8d2e60cc934df2
김호준 <didl95@naver.com>
2025-03-06 02:18:12 +0900

    refactor: 공유일기 dto 수정

--- mongeul/src/main/java/com/specup/mongeul/domain/diary/dto/request/ShareDiary/ShareDiaryUpdateRequest.java
+++ mongeul/src/main/java/com/specup/mongeul/domain/diary/dto/request/ShareDiary/ShareDiaryUpdateRequest.java
@@ -1,22 +1,23 @@
 package com.specup.mongeul.domain.diary.dto.request.ShareDiary;
 
-import com.specup.mongeul.domain.diary.dto.common.PictureLineDto;
 import com.specup.mongeul.domain.diary.entity.ENUM.DiaryFeeling;
 import com.specup.mongeul.domain.diary.entity.ENUM.DiaryWeather;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @Getter
+@Setter
 @ToString
 public class ShareDiaryUpdateRequest {
     private String title;
     private String content;
-    private String picture;
+    private MultipartFile picture;
     private LocalDate date;
-    private List<PictureLineDto> pictureLines;
+    private String pictureLines;
     private DiaryWeather weather;
     private DiaryFeeling feeling;
 }
