727fab0ce772752b36cb5e61dbf11286875f2be1
김호준 <didl95@naver.com>
2025-03-06 01:39:47 +0900

    feat: 일기 임시저장 목록 조회 id 내림차순 추가

--- mongeul/src/main/java/com/specup/mongeul/domain/diary/repository/DiaryRepository.java
+++ mongeul/src/main/java/com/specup/mongeul/domain/diary/repository/DiaryRepository.java
@@ -31,7 +31,7 @@ public interface DiaryRepository extends JpaRepository<Diary, Long> {
     /**
      * 임시저장 일기목록 조회
      */
-    List<Diary> findByUserAndPublishedOrderByDateDesc(@Param("user") User user,
+    List<Diary> findByUserAndPublishedOrderByDateDescIdDesc(@Param("user") User user,
                                                       @Param("published") Boolean published);
 
     /**
