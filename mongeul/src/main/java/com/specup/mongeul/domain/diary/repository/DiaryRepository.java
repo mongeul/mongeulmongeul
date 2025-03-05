cd8ad123a0be4961e0fa0bc199641a7be59f7640
김호준 <didl95@naver.com>
2025-03-06 01:49:55 +0900

    feat: 피드 조회 repository native query deleted = false 추가

--- mongeul/src/main/java/com/specup/mongeul/domain/diary/repository/DiaryRepository.java
+++ mongeul/src/main/java/com/specup/mongeul/domain/diary/repository/DiaryRepository.java
@@ -56,6 +56,7 @@ public interface DiaryRepository extends JpaRepository<Diary, Long> {
         WHERE private_status = 'PUBLIC'
           AND (:userId IS NULL OR user_id = :userId)
           AND published = true
+          AND deleted = false
         ORDER BY id DESC
         LIMIT :limit
     """, nativeQuery = true)
@@ -68,6 +69,7 @@ public interface DiaryRepository extends JpaRepository<Diary, Long> {
           AND (:userId IS NULL OR user_id = :userId)
           AND id < :lastDiaryId
           AND published = true
+          AND deleted = false
         ORDER BY id DESC
         LIMIT :limit
     """, nativeQuery = true)
