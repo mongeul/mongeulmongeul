bfb5321a437a0a9ef38d3d705e89fbfadea6bb45
진아영 <ay8160@daum.net>
2025-03-05 16:32:27 +0900

    rename: 피드 호출 함수명 및 타입명 변경

--- mongeul/src/lib/api/feed.ts
+++ mongeul/src/lib/api/feed.ts
@@ -1,11 +1,17 @@
-import { getFeedsList } from "@/actions/feed";
-import { FeedsResponse } from "@/types/feedTypes";
+import { getFeedDetail, getFeedList } from "@/actions/feed";
+import { FeedListResponse } from "@/types/feedTypes";
 
 // 피드 리스트 불러오기
-export async function fetchFeedsList(
+export async function fetchFeedList(
   pageSize: number,
   lastDiaryId: number | null,
   myFeed: boolean
-): Promise<FeedsResponse> {
-  return await getFeedsList(pageSize, lastDiaryId, myFeed);
+): Promise<FeedListResponse> {
+  return await getFeedList(pageSize, lastDiaryId, myFeed);
+}
+
+export async function fetchFeedDetail(
+  feedId: number
+): Promise<FeedListResponse> {
+  return await getFeedDetail(feedId);
 }
