bcccb01f172e6143580763d6874e1fea6cc50f05
최승탁 <tmdxkr5@hanmail.net>
2025-03-19 15:05:56 +0900

    chore: spring security 설정 수정

--- mongeul/src/main/java/com/specup/mongeul/global/config/SecurityConfig.java
+++ mongeul/src/main/java/com/specup/mongeul/global/config/SecurityConfig.java
@@ -41,6 +41,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers(
+                                "/**",
                                 "/api/auth/kakao",     // 카카오 로그인 URL 조회
                                 "/api/auth/kakao/**",  // 카카오 로그인 콜백
                                 "/api/drive/**"
