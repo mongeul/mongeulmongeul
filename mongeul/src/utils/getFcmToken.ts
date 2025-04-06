5d942d381a294a2bbaaecdd118d697692138ef90
seungmin <tmdals77117@naver.com>
2025-04-06 12:31:21 +0900

    feat: 알림페이지 토큰 생성

---
+++ mongeul/src/utils/getFcmToken.ts
@@ -0,0 +1,27 @@
+// src/utils/getFcmToken.ts
+import { messaging } from "@/lib/firebase-client";
+import { getToken } from "firebase/messaging";
+
+export const getFcmToken = async (): Promise<string | null> => {
+  if (!messaging) {
+    console.log("이 브라우저는 FCM을 지원하지 않아요 😢");
+    return null;
+  }
+
+  try {
+    const token = await getToken(messaging, {
+      vapidKey: process.env.NEXT_PUBLIC_FIREBASE_VAPID_KEY!,
+    });
+
+    if (token) {
+      console.log("✅ FCM 토큰:", token);
+      return token;
+    } else {
+      console.log("❌ 토큰 없음 (권한 필요)");
+      return null;
+    }
+  } catch (err) {
+    console.error("🚨 FCM 토큰 발급 오류:", err);
+    return null;
+  }
+};
