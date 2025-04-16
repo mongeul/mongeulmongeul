2f959609941eb4b3af0e687f8d1786af9ddb9017
seungmin <tmdals77117@naver.com>
2025-04-17 00:32:43 +0900

    fix: firebase 설정 바꿈

--- mongeul/src/utils/getFcmToken.ts
+++ mongeul/src/utils/getFcmToken.ts
@@ -1,8 +1,9 @@
-// src/utils/getFcmToken.ts
-import { messaging } from "@/lib/firebase-client";
+import { getMessagingInstance } from "@/lib/firebase-client";
 import { getToken } from "firebase/messaging";
 
 export const getFcmToken = async (): Promise<string | null> => {
+  const messaging = await getMessagingInstance();
+
   if (!messaging) {
     console.log("이 브라우저는 FCM을 지원하지 않아요 😢");
     return null;
